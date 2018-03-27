e59ff89681b62edebb0f3d69b54978d9812067e8
Chelsea Sanders <you@example.com>
2018-03-27 10:25:38 -0700

    boxes are rad

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+import "./style.scss";
+
 console.log("Hello Movie");
+
+//create four boxes labeled box 1-4 using only js
+//and yes you prob want to loop over some sort of an array
+
+let containerDiv = document.createElement("div");
+let rowDiv = document.createElement("div");
+
+for (let i = 0; i <= 4; i++) {
+  let columnDiv = document.createElement("div");
+  let boxDiv = document.createElement("div");
+  let headTag = document.createElement;
+}
+
+containerDiv.append(rowDiv);
